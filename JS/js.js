caa76d7d8ecb65552e1d98aeafa4c1c622fedfcf
Dzianis <dzianis888mv@gmail.com>
2022-10-14 21:31:41 +0300

    dz

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+
+var numb = prompt("Введите число: ");
+var numbst = prompt("В какую степень: ");
+
+
+// var result = numb * numb * numb; //Куб перемножаем 3 раза
+// document.write(result + ' - вычисляем куб числа: ' + numb);
+Sa = Math.pow(numb,numbst), // площадь грани
+
+
+alert(Sa);
