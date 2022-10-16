edebc33702ae343a9c38937d4263b8dd29c43db9
Dzianis <dzianis888mv@gmail.com>
2022-10-16 14:14:40 +0300

    Черновой варіант

--- JS/js.js
+++ JS/js.js
@@ -5,50 +5,71 @@ let velueCount = Number(velueCo);
 
 const velueTelephone = 50;
 const velueaAccess = 10;
-//let velueBalancFact = 0;
-
 let count = 0;
-let countNum = 0;
+let countBalance = 0;
 let result = 0;
 let x = 0;
+let resultAcss = 0;
+let countAcss = 0;
+let xAcss = 0;
+let countBalanceAcss = 0;
 
 while(x < velueCount){
-  
-result = velueTelephone + velueaAccess;
+
+result = velueTelephone;
 count += result;
-countNum =  Number(count);
+countBalance =  Number(count);
 ++x;
 
-// document.write(result);
-// document.write("<br>");
-// document.write(x);
-// document.write("<br>");
 console.log(result)
 
 }
 
-let velBalance = velueBalanc - countNum;
+let velBalance = velueBalanc - countBalance;
 
-document.write(`Сумма закупки ${countNum}, 
+document.write(`Сумма закупки ${countBalance}, 
 остаток вышего баланса составляет ${velBalance}, 
 колчичество телефонов Вы купили ${x}`);
 
+let velueAcss = prompt("Если Вы хотите докупить Аксессуары нажмите 1 \n если нет то нажмите 2");
 
-let velueAcss = prompt("Если Вы хотите докупить Аксессуары нажмите 1 \n если то нажмите 2");
+if(velueAcss < 2 && velueAcss){
+  let velueCoAcss = prompt("Сколько акссесуаров Вы хотите купить: ");
+while(xAcss < velueCoAcss){
+ 
+  resultAcss = velueaAccess;
+  countAcss += resultAcss;
+  countBalanceAcss =  Number(countAcss);
+  ++xAcss;
 
-
-switch (check) {
-  case "1":
-    check = Acss(check);
-    break;
-  case "2":
-    check = par(check);
-    break;
-  
-
-  default:
-    document.write(" Вы ввели не то действие, обновите страницу.");
 }
+
+}else{
+
+}
+
+let velBalanceaAcss = velBalance - countBalanceAcss;
+
+document.write("<br>");
+document.write(`Сумма закупки акссесуаров ${countBalanceAcss}, 
+остаток вышего баланса составляет ${velBalanceaAcss}, 
+Вы купили колчичество аксессуаров ${xAcss}`);
+
+console.log(velueBalanc);
+
+
+// switch (check) {
+//   case "1":
+//     check = acss()(check);
+//     break;
+//   case "2":
+//     check = nacss(check);
+//     break;
+
+
+//   default:
+//     document.write(" Вы ввели не то действие, обновите страницу.");
+// }
 
 // while((countNum + velueTelephone) <= velueBalanc){
   
